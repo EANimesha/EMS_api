e458cbb92bf09ae362bc07db9f605cbc51219cb9
EANimesha <nimeshadilini999@gmail.com>
2020-03-25 01:25:12 +0530

    added search and add employee routes

--- routes/employee.js
+++ routes/employee.js
@@ -6,3 +6,42 @@ exports.getAllEmployees=(req,res)=>{
         res.send(employees)
     })
 }
+
+exports.addEmployee=(req,res)=>{
+    db('employees')
+    .select('name','age','address','gender','salary','joined','dep_id')
+    .insert({
+        name:req.body.name,
+        age:req.body.age,
+        address:req.body.address,
+        gender:req.body.gender,
+        salary:req.body.salary,
+        joined:new Date(),
+        dep_id:req.body.dep_id
+    })
+    .then(()=>{
+        res.json(`added employee to db`);
+    })
+    .catch(err=>{
+        console.error(err);
+        res.json({error:err.code});
+    })
+}
+
+exports.searchEmployee=(req,res)=>{
+    db('employees')
+    .select('*')
+    .where({
+        age:req.params.age,
+    })
+    .andWhere('salary','>',`${req.params.salary}`)
+    .then(function(employees){
+        res.send(employees);
+    })
+    .catch(err=>{
+        console.error(err); 
+        res.json({error:err.code});
+    })
+
+}
+
