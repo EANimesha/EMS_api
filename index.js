2f162724e0b9d6fa2a13c5716e8cd3f2b6d3e49d
EANimesha <nimeshadilini999@gmail.com>
2020-03-24 16:01:08 +0530

    added ssl true for #1 error

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+var pg=require('pg');
 
 const {getAllEmployees,getOneEmployee}=require('./routes/employee');
 
 app.use(bodyParser.json());
 app.use(cors());
+
+pg.defaults.ssl = true;
 
 const port = process.env.PORT || 3000;
 
