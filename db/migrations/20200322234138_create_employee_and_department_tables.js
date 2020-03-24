a4e890180d8ed76474e49818dc0709c11d30d210
EANimesha <nimeshadilini999@gmail.com>
2020-03-25 01:27:45 +0530

    updated index file

--- db/migrations/20200322234138_create_employee_and_department_tables.js
+++ db/migrations/20200322234138_create_employee_and_department_tables.js
@@ -1,14 +1,18 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('departments',function(table){
-      table.increments();
+      table.increments('id');
       table.string('name').notNullable();
       table.string('location').notNullable();
   })
   .createTable('employees',function(table){
-      table.increments();
+      table.increments('id');
       table.string('name').notNullable();
+      table.integer('age').notNullable();
       table.string('address').notNullable();
+      table.enu('gender',['male','female']);
+      table.float('salary');
+      table.date('joined');
       table.integer('dep_id').references('id').inTable('departments');
   })
 };
--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const cors = require("cors");
 const app = express();
 var pg=require('pg');
 
-const {getAllEmployees,getOneEmployee}=require('./routes/employee');
+const {getAllEmployees,addEmployee,searchEmployee,getDepartmentEmployees}=require('./routes/employee');
+const {getAllDepartments,addDepartment}=require('./routes/department');
 
 app.use(bodyParser.json());
 app.use(cors());
 
-pg.defaults.ssl = true;
+// pg.defaults.ssl = true;
 
 const port = process.env.PORT || 3000;
 
 app.get('/employees',getAllEmployees);
+app.post('/employee',addEmployee);
+app.get('/employee/:age/:salary',searchEmployee);
+app.get('/employees/department/:id',getDepartmentEmployees);
 
+app.get('/departments',getAllDepartments);
+app.post('/department',addDepartment);
 
 
 app.listen(port, () => console.log(`app is running on port 3000`))
